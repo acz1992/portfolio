51de9414758ea624fda83ef321435e6735c9cb97
acz19 <acz1992@hotmail.co.uk>
2024-03-08 21:58:54 +0100

    latest changes

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -80,7 +80,7 @@ const EmailSection = () => {
 							htmlFor="email"
 							className="text-white block mb-2 text-sm font-medium"
 						>
-							Enter email
+							Enter your email
 						</label>
 						<input
 							name="email"
