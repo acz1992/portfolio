c6cf2a4592844bc7f5ec6fdb34b8f72a6a6cb05a
acz19 <acz1992@hotmail.co.uk>
2024-04-22 09:58:03 +0200

    Add temporary project pictures

--- src/app/components/Navigation/Navbar.jsx
+++ src/app/components/Navigation/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
 
 	return (
 		<nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#181818] bg-opacity-100">
-			<div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
+			<div className="flex container lg:py-4 lg:px-16 flex-wrap items-center justify-between mx-auto px-4 py-2">
 				<Link
 					href={"/"}
 					className="text-3xl md:text-2xl text-white font-semibold"
