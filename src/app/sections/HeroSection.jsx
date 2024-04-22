813fca342c747af0410a29939416a81bbdee9f10
acz19 <acz1992@hotmail.co.uk>
2024-04-22 09:06:54 +0200

    Add max-width for Hero Section

--- src/app/sections/HeroSection.jsx
+++ src/app/sections/HeroSection.jsx
@@ -9,15 +9,15 @@ import textConent from "../../data/textContent.json";
 const HeroSection = () => {
 	const { subHeader } = textConent;
 	return (
-		<section className="lg-py-16">
-			<div className="grid grid-cols-1 sm:grid-cols-12">
+		<section className="lg-py-16 flex justify-center">
+			<div className="grid grid-cols-1 sm:grid-cols-12 max-w-6xl">
 				<motion.div
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 0.5 }}
 					className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
 				>
-					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
+					<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-5xl lg:leading-normal font-extrabold">
 						<span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-800 via-primary-500 to-primary-400">
 							Hello, I&apos;m{" "}
 						</span>
@@ -36,7 +36,7 @@ const HeroSection = () => {
 							repeat={Infinity}
 						/>
 					</h1>
-					<p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+					<p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-lg">
 						{subHeader}
 					</p>
 					<div>
